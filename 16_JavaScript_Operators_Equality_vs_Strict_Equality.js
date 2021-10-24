75e03ce176aeadb0f5ee025b1f15459ead6c5f06
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 21:53:44 +0100

    Create 16_JavaScript_Operators_Equality_vs_Strict_Equality.js
    Basic exercise that checks the equality operator == compares two values after all necessary type conversions.

---
+++ 16_JavaScript_Operators_Equality_vs_Strict_Equality.js
@@ -0,0 +1,12 @@
+let varA = '10';
+let varB = 10;
+
+let varC = "";
+let varD = 0;
+let varE = 1;
+
+console.log( varA == varB);
+console.log(varA === varB);
+console.log(varC == varD);
+console.log(varC === varD);
+console.log(varC == varE);
