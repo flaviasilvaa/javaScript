83b06b34f207b243bf22a1ec4ab6d9716b05efa7
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 22:26:44 +0100

    Create 25JavaScript_Data_Structures_Array_Indexing.js
    Basic exercise that show how to access single values from an array using array indexes.

---
+++ 25JavaScript_Data_Structures_Array_Indexing.js
@@ -0,0 +1,8 @@
+let clothes = ["shirt", "shoes", "socks", "hat", "vest", "shorts", "skirt", "scarf"];
+let varOne = clothes[0];
+let varTwo = clothes[5];
+let varThree = clothes[4];
+
+console.log(varOne);
+console.log(varTwo);
+console.log(varThree);
