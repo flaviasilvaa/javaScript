580a369d90c5155062e634ede4a7af58c59022b4
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 21:17:41 +0100

    Create 4_JavaScript_Working_With_String.js
    Basic way to declare and work with strings in javaScript
    When creating strings you can use either single quotes or double quotes, as long as they're consistent

---
+++ 4_JavaScript_Working_With_String.js
@@ -0,0 +1,6 @@
+let stringWithQuotes = 'Confucius once said, "It does not matter how slowly you go, as long as you do not stop."' 
+let stringWithApostrophe = "Everything you've ever wanted is on the other side of fear." 
+
+// These lines will log your quotes to the console:
+console.log(stringWithQuotes);
+console.log(stringWithApostrophe);
