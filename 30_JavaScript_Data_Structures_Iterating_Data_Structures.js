04a4fecb6c73c914c23255cd3f47a075d5ca2757
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 22:33:44 +0100

    Create 30_JavaScript_Data_Structures_Iterating_Data_Structures.js
    Using a loop to iterate through arrays and objects.

---
+++ 30_JavaScript_Data_Structures_Iterating_Data_Structures.js
@@ -0,0 +1,11 @@
+let gamerScores = {
+    john: 89,
+    paul: 725,
+    george: 553,
+    robert: 9302,
+    steve: 733,
+};
+for (let i in gamerScores){
+    console.log(i, 'scored', gamerScores[i]);
+}
+
