4f37c4f9a93263baafcec39e76f11f35947a4f59
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 22:11:12 +0100

    Update 21_JavaScript_Iteration_For_Loops.js

--- 21_JavaScript_Iteration_For_Loops.js
+++ 21_JavaScript_Iteration_For_Loops.js
@@ -5,3 +5,15 @@ let valNumbers = numbers.length;
 for(let i = 0; i < valNumbers; i++) {
     console.log(numbers[i]+ 10);
 }
+
+
+////Another example using for and if and break///////
+for(let i = 0; i <= 20; i++){
+    if (i%2 !== 0){
+        continue;
+    }
+    if (i  == 10){
+        break;
+    }
+    console.log(i);
+}
