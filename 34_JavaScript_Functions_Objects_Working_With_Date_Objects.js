96cb2850d75a881a8514bce13d58bb6443c8eaa3
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 22:41:26 +0100

    Create 34_JavaScript_Functions_Objects_Working_With_Date_Objects.js
    Using built-in functions - working with date objects

---
+++ 34_JavaScript_Functions_Objects_Working_With_Date_Objects.js
@@ -0,0 +1,10 @@
+let today = 1597673635658;
+
+let date = new Date(today);
+
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+console.log(day);
+console.log(month);
+console.log(year);
