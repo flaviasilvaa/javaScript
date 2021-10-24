78d9a6d61f74170240d042a2b4c35a5ca0cae2a1
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 22:03:57 +0100

    Create 20_JavaScript_Flow_Control_Nested_if_else.js
    Exercise using nested if else statements inside each other to further refine the direction our program takes

---
+++ 20_JavaScript_Flow_Control_Nested_if_else.js
@@ -0,0 +1,14 @@
+let user = true;
+let emailVerified = false;
+
+
+if (user == true) {
+  if (emailVerified == true) {
+        console.log( "Welcome to our web-site");
+    } else {
+        console.log("Please verify your email");
+    }
+}
+else {
+        console.log("You need to be a user to visit this page");
+}
