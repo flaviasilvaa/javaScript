09af4e79fb881b756811f16dd03f827cfbd80a15
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 21:35:07 +0100

    Create 10_JavaScript_Data_Types_infinity_NaN.js
    Basic exercise using infinity and NaN(not a number)

---
+++ 10_JavaScript_Data_Types_infinity_NaN.js
@@ -0,0 +1,8 @@
+let notANumber = isNaN("Flavia");
+console.log(notANumber);
+let isANumber = isNaN(1234);
+console.log(isANumber);
+let finiteNumber = isFinite(12345);
+console.log(finiteNumber);
+let notAFiniteNumber = isFinite(1.9e308);
+console.log(notAFiniteNumber);
