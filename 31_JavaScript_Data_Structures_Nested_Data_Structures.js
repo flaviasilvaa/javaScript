6c449413603021defb7e05d22f7d6dd4e59cf928
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 22:35:25 +0100

    Create 31_JavaScript_Data_Structures_Nested_Data_Structures.js
    Basic exercise using nested data structures

---
+++ 31_JavaScript_Data_Structures_Nested_Data_Structures.js
@@ -0,0 +1,12 @@
+let studentData =[ {
+    name: 'John Smith',
+    email: 'john@gmail.com',
+    subjects:['Math','Psychology','Physics','Chemistry','Biology']
+},    
+{
+    name:'Mary Jones',
+    email: 'mary@gmail.com',
+    subjects: ['Fine Art', 'Music', 'Biology', 'Geography','Politics']
+}
+];
+console.log(studentData);
