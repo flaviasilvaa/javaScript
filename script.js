80a4236e594292b8878cd431ba51c7613a38374f
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-25 00:07:15 +0100

    Add files via upload
    how to use the getElementsByClassName() method to get the HTMLCollection of elements by their class attributes.

---
+++ script.js
@@ -0,0 +1,5 @@
+let mainContent = document.getElementsByClassName('main-content');
+console.log(mainContent);
+
+let second = document.getElementsByClassName('section-content')[0]
+console.log(second)
