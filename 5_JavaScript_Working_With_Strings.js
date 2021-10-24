4abe4f09b9fff82532c1b71abb6bdb23c9bd647f
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 21:21:35 +0100

    Update 5_JavaScript_Working_With_Strings.js

--- 5_JavaScript_Working_With_Strings.js
+++ 5_JavaScript_Working_With_Strings.js
@@ -2,3 +2,11 @@ let haiku = "Power down your brain\nMotion will now take over\nRide this inertia
 
 // This will log your haiku to the console:
 console.log(haiku);
+
+
+///////////////////another example/////////////////////////////
+let firstName = "Mike";
+let email = "mike@email.com";
+let message = `Dear ${firstName},\nThank you for subscribing!\nYour email: ${email}`;
+
+console.log(message)
