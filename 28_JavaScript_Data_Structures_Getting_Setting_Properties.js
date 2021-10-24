8a8b59c4166c38e56733a7703a378991d3be16bf
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 22:31:08 +0100

    Create 28_JavaScript_Data_Structures_Getting_Setting_Properties.js
    Basic exercise that show how to access and add values to an object with JavaScript

---
+++ 28_JavaScript_Data_Structures_Getting_Setting_Properties.js
@@ -0,0 +1,21 @@
+let data = {
+    firstName: "Arthur",
+    lastName: "Dent",
+    species: "Human"
+};
+
+
+let firstName = data["firstName"];
+console.log(firstName);
+
+let species = data.species;
+console.log(data.species)
+
+data.age = 42;
+
+
+
+
+
+// this will log the data to the console
+console.log(data);
