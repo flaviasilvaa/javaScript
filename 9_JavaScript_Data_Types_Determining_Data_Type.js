1818b2a23020fb85efffda5a3de2f40ddf988538
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 21:27:34 +0100

    Create 9_JavaScript_Data_Types_Determining_Data_Type.js
    In this exercise is working with the typof  operator to find out the different types of data we can work with
    log the types for 3 different variables to the console using the typeof operator.

---
+++ 9_JavaScript_Data_Types_Determining_Data_Type.js
@@ -0,0 +1,7 @@
+let itmOne = 10;
+let itmTwo = '20';
+let itmThree = true;
+console.log(typeof itmOne);
+console.log(typeof itmTwo);
+console.log(typeof itmThree);
+console.log(typeof (itmOne + itmTwo));
