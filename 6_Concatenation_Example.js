715aece95b8c6f16dd485d3507bd015a22550b80
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 21:24:34 +0100

    Update 6_Concatenation_Example.js

--- 6_Concatenation_Example.js
+++ 6_Concatenation_Example.js
@@ -1,3 +1,19 @@
 let username = "Jack";
 let message = "Welcome back" + ' ' + username +  "! You are now logged in.";
 console.log(message);
+
+
+////another example////////////////
+
+/ These variables will be sliced and concatinated
+// to form the emailAddress variable:
+let fullName = 'Andrew Martin';
+let domainName = 'gmail';
+let tld = 'com';
+
+let emailAddress = 
+// Remember to use the slice() and toLowerCase() string methods:
+
+
+// This will log the emailAddress variable to the console
+console.log(emailAddress);
