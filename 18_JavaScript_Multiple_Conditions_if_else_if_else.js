c03ab5d562694519baa24188e45c6b8a10b9fea5
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 22:00:45 +0100

    Create 18_JavaScript_Multiple_Conditions_if_else_if_else.js
    Basic exercise that controls the flow of your program based on multiple conditions.

---
+++ 18_JavaScript_Multiple_Conditions_if_else_if_else.js
@@ -0,0 +1,12 @@
+let age = 15;
+let result;
+if (age > 21) {
+    result = "drink whiskey";
+}else if (age >= 18) {
+    result = "drink beer";
+}else if (age > 12) {
+    result = "drink cola"
+}else {
+    result = "drink apple juice";
+}
+console.log(result);
