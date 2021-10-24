d25dc9f069764e6835de7533dbadd75261fe1030
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 21:12:00 +0100

    Create 2_JavaScript_Variables_Variable_Scope.js
    the use of declaring variables in JavaScript.

---
+++ 2_JavaScript_Variables_Variable_Scope.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const company = "Big Bucks co.";
+let profit = 900;
+let financeManager = "Richard";
+
+if (profit < 1000) {
+	var richardFired = true;
+	financeManager = "Fay";
+} 
+
+console.log(company);
+console.log(financeManager);
+console.log(richardFired);
