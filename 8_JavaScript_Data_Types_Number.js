5492a1054ccdef4d92ec6812685566934f41945f
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-24 21:31:13 +0100

    Create 8_JavaScript_Data_Types_Number.js
    Basic exercise using Number methods to manipulate and retrieve data about numbers in JavaScript

---
+++ 8_JavaScript_Data_Types_Number.js
@@ -0,0 +1,8 @@
+let myStringInt = '10';
+let myStringFloat = '1.275';
+
+// Fix this code so that it adds correctly
+let result = parseInt(myStringInt) + parseFloat(myStringFloat);
+
+// This will print your results to the console
+console.log(result);
