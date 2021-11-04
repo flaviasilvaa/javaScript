3ac668b3e6a83dd100933cc0a15001b22f989a75
flaviasilvaa <flaviasilvaa@gmail.com>
2021-11-04 22:54:03 +0000

    Create 42_JavaScript_RND_Parents_Children_Siblings.js

---
+++ 42_JavaScript_RND_Parents_Children_Siblings.js
@@ -0,0 +1,13 @@
+let parentElement = document.getElementsByClassName('section-content')[0].parentNode;
+
+console.log(parentElement);
+
+let children = document.getElementById('main').children;
+
+console.log(children);
+
+let sibling = document.getElementsByTagName("h1")[0].nextElementSibling;
+
+console.log(sibling);
+
+
